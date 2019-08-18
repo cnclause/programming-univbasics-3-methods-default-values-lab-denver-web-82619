ef87cdedd13d290c1c2f0f773a4535b96b8c36e7
Catherine Clause <cnclause123@gmail.com>
2019-08-18 22:55:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@
 def meal_choice(veg1, veg2, protein = "meat") 
 order = "A plate of #{protein} with #{veg1} and #{veg2}."
   p order 
-  return "What a nutritious meal #{order}"
+  p "What a nutritious meal #{order}"
 end 
 
