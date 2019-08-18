43ebc7b35618ac71fcc958db38fa5488cf6c512a
Catherine Clause <cnclause123@gmail.com>
2019-08-18 22:14:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 # Your code here
-puts ""
+puts "What a nutritious meal! mean_choice"
 def meal_choice(veg1, veg2, protein = "meat") 
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
