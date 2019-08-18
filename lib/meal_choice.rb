ee30c3bf08ce41d519afac609bf71d9c45e8af8f
Catherine Clause <cnclause123@gmail.com>
2019-08-18 23:10:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 # Your code here
 
 def meal_choice(veg1, veg2, protein = "meat") 
-nutrition = "What a nutritious meal!" 
+order = "What a nutritious meal!" 
  return "#{nutrition} A plate of #{protein} with #{veg1} and #{veg2}." 
 end 
 
