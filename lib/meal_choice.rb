44926351e79b213f437a9d68fdadf171477a47a7
Catherine Clause <cnclause123@gmail.com>
2019-08-18 23:14:22 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,9 @@
 # Your code here
 
 def meal_choice(veg1, veg2, protein = "meat") 
+  puts "What a nutritious meal! 
 order = "A plate of #{protein} with #{veg1} and #{veg2}." 
 puts order
- return "What a nutritious meal! #{order}"
-end 
+ return order
+end
 
